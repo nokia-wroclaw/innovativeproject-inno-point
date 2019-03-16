c3ace2c1a2b4a6cbd94ebf9a0e15f00481ba8c97
Imperative2 <nie335@gmail.com>
2019-03-16 12:01:45 +0100

    some setup added to new method of authorization

--- server/services/GitHubCalls.js
+++ server/services/GitHubCalls.js
@@ -1,4 +1,5 @@
 var https = require('https');
+var crypto = require('crypto');
  
 class GitHubCalls{
 
@@ -33,6 +34,59 @@ class GitHubCalls{
             
         });
    
+    }
+
+    gitAuthWithToken(OAuthToken){
+        let optionsget = {
+            host : 'api.github.com',
+            port : 443,
+            path : '/user', 
+            method : 'GET', 
+            headers: {'user-agent': 'node.js', 'Authorization' : 'token ' + OAuthToken}
+        };
+
+        return new Promise((resolve, reject) => {
+            let reqGet = https.request(optionsget, function(res){
+                res.on('data', function(d) {
+                    console.info('GET result:\n');
+                    process.stdout.write(d);
+                    console.info('\n\nCall completed');
+                });
+                resolve(res.statusCode);
+            })
+
+            reqGet.end();
+            reqGet.on('error',function(e){
+                reject(err);
+            })
+            
+        });
+   
+    }
+
+    gitAuthWithCallback(callback){
+
+        var unGuessableState = crypto.createHash('sha256');
+        unGuessableState.update(Math.random().toString());
+
+        console.log("hash: " + unGuessableState.digest());
+
+        let optionsget = {
+            host : 'github.com',
+            port : 443,
+            path : '/login/oauth/authorize', 
+            method : 'GET', 
+            headers: {'user-agent': 'node.js', 'Authorization' : 'Basic ' + base64Auth},
+            client_id: 'cb8b7f9b4e0a03ead294',
+            redirect_uri: callback,
+            scope: '',
+            state: unGuessableState,
+            allow_signup: 'true'
+        };
+
+        let reqGet = https.request(optionsget, )
+
+        return unGuessableState;
     }
 
 }
