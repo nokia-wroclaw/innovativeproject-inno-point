4b9c6b25fc2361177b13359cad194e10b427fb79
Imperative2 <nie335@gmail.com>
2019-03-24 21:16:41 +0100

    Added routes for github request callback

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,11 @@
 const projectRoutes = require("./projects");
 const userRoutes = require("./users");
+const githubRoutes = require("./github");
 
 const initializeRoutes = app => {
   projectRoutes(app);
   userRoutes(app);
+  githubRoutes(app);
 };
 
 module.exports = initializeRoutes;
