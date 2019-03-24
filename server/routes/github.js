9f002bc43defbca2002e32173615602cc89224a9
Dominik Sławkowski <dominik.slawkowski@gmail.com>
2019-03-24 22:33:53 +0100

    small fix

--- server/routes/github.js
+++ server/routes/github.js
@@ -1,26 +1,22 @@
 const gitHub = require("../services/GitHubCalls");
 
 const gitHubRoutes = app => {
-    app.get("/auth/callback", (req, res) => {
-        const {query} = req;
+  app.get("/auth/callback", (req, res) => {
+    const { query } = req;
 
-        const { code } = query;
+    const { code } = query;
 
-        if(!code){
-            return res.send({
-                sucess: false,
-                message: 'Error: no code'
-            })
-        }
+    if (!code) {
+      return res.send({
+        sucess: false,
+        message: "Error: no code"
+      });
+    }
 
+    console.log("code", code);
+  });
 
-        console.log('code', code);
-
-    });
-
-    app.get("/", (req, res) => {
-
-    });
+  app.get("/", (req, res) => {});
 };
 
-module.export = gitHubRoutes;
+module.exports = gitHubRoutes;
