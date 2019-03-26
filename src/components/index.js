40ddb63a4fa282bb02db89c1af88123269329d7b
Artur Fracala <arturfracala@gmail.com>
2019-03-26 20:41:41 +0100

    add new component - button for sending emails

--- src/components/index.js
+++ src/components/index.js
@@ -14,6 +14,7 @@ import GoalsBlock from "./Project/GoalsBlock";
 import ScopesBlock from "./Project/ScopesBlock";
 import TechnologyBlock from "./Project/TechnologyBlock";
 import TagsBlock from "./Project/TagsBlock";
+import MailButton from "./MailButton";
 
 export {
   News,
@@ -32,5 +33,6 @@ export {
   GoalsBlock,
   ScopesBlock,
   TechnologyBlock,
-  TagsBlock
+  TagsBlock,
+  MailButton
 };
