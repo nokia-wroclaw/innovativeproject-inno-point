6e60842ec4688b80010c1bda407c310bf9274816
Dominik Slawkowski <dominik.slawkowski@gmail.com>
2019-06-11 20:57:23 +0200

    fix

--- server/models/report.js
+++ server/models/report.js
@@ -1,26 +1,33 @@
 module.exports = function(sequelize, DataTypes) {
-  return sequelize.define("report", {
-    id: {
-      type: DataTypes.INTEGER(11).UNSIGNED,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true
-    },
-    user_id: {
-      type: DataTypes.INTEGER(11).UNSIGNED,
-      allowNull: false,
-      references: {
-        model: "user",
-        key: "id"
+  return sequelize.define(
+    "report",
+    {
+      id: {
+        type: DataTypes.INTEGER(11).UNSIGNED,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      user_id: {
+        type: DataTypes.INTEGER(11).UNSIGNED,
+        allowNull: false,
+        references: {
+          model: "user",
+          key: "id"
+        }
+      },
+      message: {
+        type: DataTypes.TEXT,
+        allowNull: true
+      },
+      type: {
+        type: DataTypes.CHAR(30),
+        allowNull: false
       }
     },
-    message: {
-      type: DataTypes.TEXT,
-      allowNull: true
-    },
-    type: {
-      type: DataTypes.CHAR(30),
-      allowNull: false
+    {
+      tableName: "report",
+      timestamps: false
     }
-  });
+  );
 };
