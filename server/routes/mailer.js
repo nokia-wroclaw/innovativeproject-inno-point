c239cbd6704899cef3b069fd58113f37c943a060
Imperative2 <nie335@gmail.com>
2019-06-11 22:15:22 +0200

    new icon email auth emails routes

--- server/routes/mailer.js
+++ server/routes/mailer.js
@@ -5,19 +5,32 @@ const mailServiceRoutes = app => {
   app.put("/sendMail/requestMentorStatus", (req, res) => {
     //    console.log(req.body);
 
-    const mailService = new MailService();
-    const DB = new DBConnection();
+    const token = req.body;
+    jwt.verify(token, config.jwt.secretkey, (err, authData) => {
+      if (err) {
+        res.sendStatus(403);
+      } else {
+        const { id } = authData;
+        clearanceCheck.isDeveloperUp(id).then(result => {
+          if (result == false) res.sets(403);
+          else {
+            const mailService = new MailService();
+            const DB = new DBConnection();
 
-    DB.getModeratorEmails().then(moderatorsEmails => {
-      const data = {
-        userName: req.body.userName,
-        userId: req.body.userId,
-        recipientEmails: moderatorsEmails
-      };
+            DB.getModeratorEmails().then(moderatorsEmails => {
+              const data = {
+                userName: req.body.userName,
+                userId: req.body.userId,
+                recipientEmails: moderatorsEmails
+              };
 
-      mailService.requestMentorStatus(data).then(() => {
-        res.send("mail sent");
-      });
+              mailService.requestMentorStatus(data).then(() => {
+                res.send("mail sent");
+              });
+            });
+          }
+        });
+      }
     });
   });
 
@@ -27,16 +40,29 @@ const mailServiceRoutes = app => {
     const mailService = new MailService();
     const DB = new DBConnection();
 
-    DB.getModeratorEmails().then(moderatorsEmails => {
-      const data = {
-        projectName: req.body.projectName,
-        projectId: req.body.projectId,
-        recipientEmails: moderatorsEmails
-      };
+    const token = req.body;
+    jwt.verify(token, config.jwt.secretkey, (err, authData) => {
+      if (err) {
+        res.sendStatus(403);
+      } else {
+        const { id } = authData;
+        clearanceCheck.isDeveloperUp(id).then(result => {
+          if (result == false) res.sets(403);
+          else {
+            DB.getModeratorEmails().then(moderatorsEmails => {
+              const data = {
+                projectName: req.body.projectName,
+                projectId: req.body.projectId,
+                recipientEmails: moderatorsEmails
+              };
 
-      mailService.requestTopicReview(data).then(() => {
-        res.send("mail sent");
-      });
+              mailService.requestTopicReview(data).then(() => {
+                res.send("mail sent");
+              });
+            });
+          }
+        });
+      }
     });
   });
 };
